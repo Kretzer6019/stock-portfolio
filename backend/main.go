3b02cddbe2cc8ed5f509c66d65cc617039d0714e
Kretzer6019 <vitor.kretzer@saleshunter.ai>
2023-02-06 23:48:41 -0300

    create .env

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"dependencies/router"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Get env variables
+	err := godotenv.Load("../config.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	// Generate all routes
 	r := router.Generate()
 
