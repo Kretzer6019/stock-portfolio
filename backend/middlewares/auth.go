8841a7056b1f7019d6264e04f1fa6d2c205c7e0b
Kretzer6019 <vitor.kretzer@saleshunter.ai>
2023-04-09 12:00:04 -0300

    improve auth middleware

--- backend/middlewares/auth.go
+++ backend/middlewares/auth.go
@@ -2,15 +2,26 @@ package middlewares
 
 import (
 	"dependencies/auth"
+	"dependencies/models/users"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		err := auth.ValidateToken(c)
+		userID, err := auth.ExtractUserID(c)
+		if err != nil {
+			log.Println("Error:", err)
+			c.AbortWithStatus(http.StatusUnprocessableEntity)
+			return
+		}
+		// Connect to db
+		db := c.MustGet("db").(*gorm.DB)
+		// Check if user exists
+		_, err = users.SelectUser(int(userID), db)
 		if err != nil {
 			log.Println("Error:", err)
 			c.AbortWithStatus(http.StatusUnprocessableEntity)
